ceb9b6c07688fc50926b2df0370f1fbf0a397a12
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-09-16 15:27:43 +0500

    learn python start

---
+++ learn.py
@@ -0,0 +1,25 @@
+# print("Hello world!")
+
+# def add(a, b):
+#     return a + b
+
+# print(add(2, 3))
+
+# for i in range(5):
+#     print(i)
+
+
+# my_list = [1, 2, 3, 4, 5]
+
+# dict_example = {'a': 1, 'b': 2, 'c': 3}
+# print(dict_example)
+
+# print("This is a sample Python script.")
+# print("It demonstrates basic syntax and features of Python.")
+
+
+if 6 > 2:
+    print("Six is greater than two!")
+else:
+    print("Six is not greater than two.")
+    
